2d24ddc205e81bdb87bbe5cdaa894c31aa6f63ee
Alexander Kostrikin <aleksandrkostrikin086@gmail.com>
2023-08-22 11:25:51 +0300

    Add UserFactory, setup DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,9 +20,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([
-            UsersTableSeeder::class,
-            UsersInfoTableSeeder::class
-        ]);
+//        $this->call([
+//            UsersTableSeeder::class,
+//            UsersInfoTableSeeder::class
+//        ]);
+        User::factory()->count(20)->create();
     }
 }
